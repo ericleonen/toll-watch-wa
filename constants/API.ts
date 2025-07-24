2f0d5b255d03143499e4b17b38aaa2149c3772d6
ericleonen <ericleonen@gmail.com>
2025-07-24 11:01:45 -0700

    add obfuscated client API key

--- constants/API.ts
+++ constants/API.ts
@@ -1,2 +1,2 @@
 export const TOLL_WATCH_UPCOMING_URL = "https://toll-watch-wa.onrender.com/upcomingTolls"
-export const OBFUSCATED_TOLLWATCH_API_KEY = "..."
+export const OBFUSCATED_TOLLWATCH_API_KEY = "MVlrTURqSVU3ZlNCaDJLVVU3ZHhyZGRrY3pHYjJVdWQxa2pMMHB6dnhPcUY="
